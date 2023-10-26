b1102d286ccc2c71ce424dfd9c187c1700f40a0d
chamodSamajith <chamodsr@gmail.com>
2023-10-27 00:22:18 +1100

    add tutor UI updates and API integrations

--- src/containers/tutor/AddTutor.js
+++ src/containers/tutor/AddTutor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, TextField, Typography, InputLabel, Select, MenuItem, Button } from "@mui/material";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -6,7 +6,19 @@ import toast from "react-hot-toast";
 const AddTutor = () => {
   const [tutorName, setTutorName] = useState("");
   const [subject, setSubject] = useState([]);
-  const [dateTime, setDateTime] = useState("");
+  const [dateTimeFrom, setDateTimeFrom] = useState("");
+  const [dateTimeTo, setDateTimeTo] = useState("");
+  const [tutors, setTutors] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3200/api/tutors/getAllTutorInfo")
+      .then((response) => {
+        setTutors(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tutors: ", error);
+      });
+  }, []);
 
   const onHandleName = (e) => {
     setTutorName(e.target.value);
@@ -16,27 +28,48 @@ const AddTutor = () => {
     setSubject(e.target.value);
   };
 
-  const onHandleDate = (e) => {
-    setDateTime(e.target.value);
+  const onHandleDateFrom = (e) => {
+    setDateTimeFrom(e.target.value);
+  };
+
+  const onHandleDateTo = (e) => {
+    setDateTimeTo(e.target.value);
   };
 
   const validateForm = () => {
-    if (!tutorName || subject.length === 0 || !dateTime) {
+    if (!tutorName || subject.length === 0 || !dateTimeFrom || !dateTimeTo) {
       toast.error("Please fill in all fields");
       return false;
     }
+
+    // Check if dateTimeFrom is earlier than dateTimeTo
+    if (new Date(dateTimeFrom) >= new Date(dateTimeTo)) {
+      toast.error("Date and time 'From' must be earlier than 'To'");
+      return false;
+    }
+
     return true;
   };
 
   const sendApiRequest = () => {
+    // Format the date and time values
+    const formattedDateTimeFrom = new Date(dateTimeFrom).toISOString();
+    const formattedDateTimeTo = new Date(dateTimeTo).toISOString();
+    console.log(dateTimeFrom,dateTimeTo)
     axios
       .post("http://localhost:3200/api/tutordetails/create", {
         TutorName: tutorName,
         TutorSubjects: subject,
-        AvailableDateTIme: dateTime,
+        AvailableDateTimeFrom: dateTimeFrom,
+        AvailableDateTimeTo: dateTimeTo,
       })
       .then((response) => {
         toast.success("Tutor details inserted successfully");
+        // Clear input fields
+        setTutorName("");
+        setSubject([]);
+        setDateTimeFrom("");
+        setDateTimeTo("");
       })
       .catch((error) => {
         toast.error("Error creating tutor");
@@ -47,10 +80,6 @@ const AddTutor = () => {
     e.preventDefault();
     if (validateForm()) {
       sendApiRequest();
-      // Clear input fields
-    setTutorName("");
-    setSubject([]);
-    setDateTime("");
     }
   };
 
@@ -63,15 +92,23 @@ const AddTutor = () => {
           </Typography>
 
           <Grid style={{ marginTop: "2%" }} item sm={6} xs={12}>
-            <TextField
-              size="medium"
-              fullWidth
-              label="Tutor Name"
-              name="name"
-              value={tutorName}
-              onChange={onHandleName}
-            />
-          </Grid>
+  <InputLabel>Tutor Name</InputLabel>
+  <Select
+     fullWidth
+    label="Tutor Name"
+    value={tutorName}
+    onChange={onHandleName}
+  >
+    <MenuItem value="">
+      <em>None</em>
+    </MenuItem>
+    {tutors.map((tutor) => (
+      <MenuItem key={tutor._id} value={tutor.TutorFName}>
+        {tutor.TutorFName} {tutor.TutorLName}
+      </MenuItem>
+    ))}
+  </Select>
+</Grid>
 
           <Grid style={{ marginTop: "1%" }} item sm={6} xs={12}>
             <InputLabel>Subject Expertise</InputLabel>
@@ -93,15 +130,26 @@ const AddTutor = () => {
           </Grid>
 
           <Grid style={{ marginTop: "2%" }} item sm={6} xs={12}>
-            <InputLabel id="demo-select-small-label">Date Available</InputLabel>
+            <InputLabel>Date Available From</InputLabel>
             <TextField
               size="medium"
               fullWidth
-              label=""
-              name="avDate"
-              value={dateTime}
+              name="dateTimeFrom"
+              value={dateTimeFrom}
               type="datetime-local"
-              onChange={onHandleDate}
+              onChange={onHandleDateFrom}
+            />
+          </Grid>
+
+          <Grid style={{ marginTop: "2%" }} item sm={6} xs={12}>
+            <InputLabel>Date Available To</InputLabel>
+            <TextField
+              size="medium"
+              fullWidth
+              name="dateTimeTo"
+              value={dateTimeTo}
+              type="datetime-local"
+              onChange={onHandleDateTo}
             />
           </Grid>
 
--- src/containers/tutor/TutorSheduling.js
+++ src/containers/tutor/TutorSheduling.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import DataTable from 'react-data-table-component';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -6,82 +6,94 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
+import axios from 'axios';
 
 const TutorSheduling = () => {
+  const [data, setData] = useState([]);
+  const [selectedRowData, setSelectedRowData] = useState(null);
 
-    const columns = [
-        {
-            name: 'Title',
-            selector: row => row.title,
-        },
-        {
-            name: 'Year',
-            selector: row => row.year,
-        },
-    ];
+  const columns = [
+    {
+      name: 'Session Name',
+      selector: (row) => row.sessionName,
+    },
+    {
+      name: 'Start Time',
+      selector: (row) => row.startTime,
+    },
+    {
+      name: 'End Time',
+      selector: (row) => row.endTime,
+    },
+  ];
 
-    const data = [
-        {
-            id: 1,
-            title: 'Beetlejuice',
-            year: '1988',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-    ]
-
-    const handleRowClick = (row, event) => {
-        console.log(row)//can access values
+  const fetchSessionData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3200/api/session/all');
+      setData(response.data.data);
+    } catch (error) {
+      console.error('Error fetching session data', error);
     }
+  };
 
-    return (
-        <div>
-            <div>
-                <DataTable
-                    onRowClicked={handleRowClick}
-                    columns={columns}
-                    data={data}
-                />
-            </div>
+  useEffect(() => {
+    fetchSessionData();
+  }, []);
 
-            <div style={{ marginTop: "5%" }}>
-                <hr />
-                {/* filter and display availabe tutors for session( session displayed in row) */}
-                <h3>Avalable Tutors</h3>
-                <p style={{ marginTop: "1%" }}>Please Click on a Session to See Available tutors for that Session</p>
+  const handleRowClick = (row) => {
+    setSelectedRowData(row);
+  };
 
+  const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const formattedDate = dateTime.toLocaleDateString(); 
+    const formattedTime = dateTime.toLocaleTimeString(); 
+    return `${formattedDate} ${formattedTime}`;
+  };
 
-                <div>
-                    {/* filter and display tutors */}
-                    <Card sx={{ minWidth: 275, maxWidth: '300px' }}>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                Word of the Day
-        </Typography>
-                            <Typography variant="h5" component="div">
-                                test test st
-        </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                adjective
-        </Typography>
-                            <Typography variant="body2">
-                                well meaning and kindly.
-          <br />
-                                {'"a benevolent smile"'}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="medium">Assign</Button>
-                        </CardActions>
-                    </Card>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <DataTable
+          onRowClicked={handleRowClick}
+          columns={columns}
+          data={data}
+        />
+      </div>
 
-export default TutorSheduling
+      <div style={{ marginTop: '5%' }}>
+        <hr />
+        <h3>Available Tutors</h3>
+        <p style={{ marginTop: '1%' }}>
+          Please Click on a Session to See Available tutors for that Session
+        </p>
+
+        {selectedRowData && (
+          <Card sx={{ minWidth: 275, maxWidth: '300px' }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {selectedRowData.sessionName}
+              </Typography>
+              <Typography variant="h5" component="div">
+                FROM: {formatDateTime(selectedRowData.startTime)} TO: {formatDateTime(selectedRowData.endTime)}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Session Subject: {selectedRowData.sessionSubject}
+              </Typography>
+              <Typography variant="body2">
+                Maximum Participants: {selectedRowData.maximumParticipants}
+                <br />
+                Status: {selectedRowData.status ? 'Active' : 'Inactive'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="medium">Assign</Button>
+            </CardActions>
+          </Card>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TutorSheduling;
