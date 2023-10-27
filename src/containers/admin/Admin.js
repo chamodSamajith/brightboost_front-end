2bb854c30fb6a749078c34066d58637d05715c1f
thanujachamika <thanujachamikatc@gmail.com>
2023-10-27 14:42:56 +1100

    Admin.js file changed

--- src/containers/admin/Admin.js
+++ src/containers/admin/Admin.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import Statistics from "./statistics/statistics";
 import Dashboard from './dashboard/Dashboard';
 const Admin = () => {
 
     return (
         <Box
-        sx={{
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Typography align="center" variant="h5">
-            <Dashboard/>
-            {/*<QuestionTable/>*/}
-        </Typography>
-      </Box>
+            sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Typography align="center" variant="h5">
+                <Dashboard/>
+            </Typography>
+        </Box>
     )
 
-  };
-  
+};
+
   export default Admin;
